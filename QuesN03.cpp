2d0f6d14438e397781f4d9d9456095f81b9d7447
ASAD AQEEL <144030369+Asad-aqeel@users.noreply.github.com>
2023-11-04 02:27:41 -0700

    Update QuesN03.cpp

--- QuesN03.cpp
+++ QuesN03.cpp
@@ -11,5 +11,41 @@ int main()
         }
         cout << endl;
     }
+
+    cout << endl;
+    cout << "Using while loop. ";
+    cout << endl;
+
+    int i = 1;
+    int j = 1;
+    while (i <= 6)
+    {
+        j = 1;
+        while (j <= 6)
+        {
+            cout << i * j << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+    cout << endl;
+    cout << "Using  do-while loop. ";
+    cout << endl;
+
+    i = 1;
+    do
+    {
+        j = 1;
+        do
+        {
+            cout << i * j << " ";
+            j++;
+        } while (j <= 6);
+        cout << endl;
+        i++;
+    } while (i <= 6);
+
     return 0;
 }
